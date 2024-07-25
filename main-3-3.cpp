579a7223335896275a84f410c128641ed08ed216
tommy wu <tommywu@tommydeMacBook-Pro.local>
2024-07-25 22:56:10 +0930

    p1

--- main-3-3.cpp
+++ main-3-3.cpp
@@ -1,10 +1,6 @@
 #include<iostream>
 double weighted_average(int array[], int n);
 int main() {
-    if (array[] = {1, 2, 1, 4, 1, 3})
-    {
-        std::cout<<"3.0"<<std::endl;
-    }
     int array[] = {1, 2, 1, 4, 1, 3};
     int n = sizeof(array) / sizeof(array[0]);
     
