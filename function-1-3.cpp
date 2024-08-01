460141d843907f2c7d3c6d6ede6a87792376b911
tommy wu <tommywu@tommydeMacBook-Pro.local>
2024-08-01 18:28:03 +0930

    dq

--- function-1-3.cpp
+++ function-1-3.cpp
@@ -1,25 +1,23 @@
 #include <iostream>
-using namespace std;
+#include <vector>
 
 void count_digits(int array[4][4]) {
-    int count[10] = {0}; 
+    
+    std::vector<int> counts(10, 0);
 
     
     for (int i = 0; i < 4; ++i) {
         for (int j = 0; j < 4; ++j) {
             int value = array[i][j];
             if (value >= 0 && value <= 9) {
-                count[value]++;
+                counts[value]++;
             }
         }
     }
 
     
     for (int i = 0; i < 10; ++i) {
-        cout << i << ":" << count[i];
-        if (i < 9) {
-            cout << ";";
-        }
+        std::cout << i << ":" << counts[i] << ";";
     }
-    cout << endl;
+    std::cout << std::endl;
 }
--- function-2-2.cpp
+++ function-2-2.cpp
@@ -1,19 +1,12 @@
-#include <cmath>
+#include <iostream>
 
+// Function to convert a binary number represented as an array to its integer value
 int binary_to_int(int binary_digits[], int number_of_digits) {
-    if (number_of_digits <= 0 || number_of_digits > 30) {
-        return -1; 
-    }
-    
-    int integer_value = 0;
-    
-    
+    int result = 0;
+
     for (int i = 0; i < number_of_digits; ++i) {
-        if (binary_digits[i] != 0 && binary_digits[i] != 1) {
-            return -1; 
-        }
-        integer_value += binary_digits[i] * pow(2, number_of_digits - 1 - i);
+        result = result * 2 + binary_digits[i];
     }
-    
-    return integer_value;
+
+    return result;
 }
