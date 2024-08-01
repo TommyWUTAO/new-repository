f47043ba17146d8c2ab5b78dff95b9dfaec2f93b
tommy wu <tommywu@tommydeMacBook-Pro.local>
2024-08-01 18:17:51 +0930

    dqw

--- function-2-2.cpp
+++ function-2-2.cpp
@@ -1,11 +1,19 @@
 #include <cmath>
-int sum_if_palindrome (int integers[], int length)
-{
-    int integer_value = 0;
-    for (int i = 0; i < length; ++i) {
-        if (integers[i] == 1) {
-            integer_value += pow(2, length - 1 - i);
-        }
+
+int binary_to_int(int binary_digits[], int number_of_digits) {
+    if (number_of_digits <= 0 || number_of_digits > 30) {
+        return -1; 
     }
+    
+    int integer_value = 0;
+    
+    
+    for (int i = 0; i < number_of_digits; ++i) {
+        if (binary_digits[i] != 0 && binary_digits[i] != 1) {
+            return -1; 
+        }
+        integer_value += binary_digits[i] * pow(2, number_of_digits - 1 - i);
+    }
+    
     return integer_value;
 }
--- function-2-4.cpp
+++ function-2-4.cpp
@@ -1,4 +1,6 @@
 int array_min(int integers[], int length) {
+    if (length <= 0) return -1; 
+    
     int min = integers[0];
     for (int i = 1; i < length; ++i) {
         if (integers[i] < min) {
@@ -8,7 +10,10 @@ int array_min(int integers[], int length) {
     return min;
 }
 
+
 int array_max(int integers[], int length) {
+    if (length <= 0) return -1; 
+    
     int max = integers[0];
     for (int i = 1; i < length; ++i) {
         if (integers[i] > max) {
@@ -18,17 +23,14 @@ int array_max(int integers[], int length) {
     return max;
 }
 
+
 int sum_min_max(int integers[], int length) {
-    if (length <= 0) {
-        return -1;
-    }
-    for (int i = 0; i < length - 1; ++i) {
-        if (integers[i] < integers[i + 1]) {
-            return -1;
-        }
-    }
-    int maximum = array_max(integers, length);
-    int minimum = array_min(integers, length);
-    int sum = maximum + minimum;
-    return sum;
+    if (length <= 0) return -1; 
+    
+    int min = array_min(integers, length);
+    int max = array_max(integers, length);
+    
+    if (min == -1 || max == -1) return -1; 
+    
+    return min + max;
 }
