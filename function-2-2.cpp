2c1980be0e74ae5bb609c66662381f16ecd63f58
tommy wu <tommywu@tommydeMacBook-Pro.local>
2024-08-01 18:31:00 +0930

    dq

--- function-2-2.cpp
+++ function-2-2.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
-
-// Function to convert a binary number represented as an array to its integer value
 int binary_to_int(int binary_digits[], int number_of_digits) {
     int result = 0;
-
+    if(number_of_digits<30&&number_of_digits>1){
     for (int i = 0; i < number_of_digits; ++i) {
         result = result * 2 + binary_digits[i];
+    }
     }
 
     return result;
