5b26df03b60e173c59464b25309c57c20274f5bd
tommy wu <tommywu@tommydeMacBook-Pro.local>
2024-08-09 08:53:54 +0930

    dqw

--- main-1-3.cpp
+++ main-1-3.cpp
@@ -5,7 +5,8 @@ double* duplicateArray(double* array, int size);
 int main() {
 
     double originalArray[] = {1.1, 2.2, 3.3, 4.4, 5.5};
-    int size = sizeof(originalArray) / sizeof(originalArray[0]);    double* newArray = duplicateArray(originalArray, size);
+    int size = sizeof(originalArray) / sizeof(originalArray[0]);    
+    double* newArray = duplicateArray(originalArray, size);
 
     std::cout << "Original array: ";
     for (int i = 0; i < size; ++i) {
