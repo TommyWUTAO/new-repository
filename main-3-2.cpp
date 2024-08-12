45a41392cb78d68114845cd8c80b121c3d09689b
tommy wu <tommywu@tommydeMacBook-Pro.local>
2024-08-12 16:28:47 +0930

    dqw

--- main-3-2.cpp
+++ main-3-2.cpp
@@ -4,6 +4,13 @@ int* readNumbers();
 int *reverseArray(int *numbers1,int length) ;
 bool equalsArray(int* numbers1, int* numbers2, int length);
 int main() {
+    int numbers1[]={1,2,3,4,5};
+    int numbers2[]={1,2,3,4,5};
+    cout<<equalsArray(numbers1,numbers2,5);
+    int numbers3[]={4,5};
+    int numbers4[]={1,2};
+    cout<<equalsArray(numbers3,numbers4,2);
+
     int numbers[] = {1, 2, 3, 4, 5};
     int length = sizeof(numbers) / sizeof(numbers[0]);
 
