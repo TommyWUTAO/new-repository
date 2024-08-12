0f0787c2b9a8abdc8969f51abb22fb868f94b4f1
tommy wu <tommywu@tommydeMacBook-Pro.local>
2024-08-12 16:24:39 +0930

    fw

--- function-3-2.cpp
+++ function-3-2.cpp
@@ -8,6 +8,17 @@ int* readNumbers() {
     return numbers;
 }
 
+bool equalsArray(int* numbers1, int* numbers2, int length) {
+    if (length < 1) {
+        return false;
+    }
+    for (int i = 0; i < length; ++i) {
+        if (numbers1[i] != numbers2[i]) {
+            return false;
+        }
+    }
+    return true;
+}
 int *reverseArray(int *numbers1,int length){
     int* arr=new int[length];
     for(int i=0;i<length;i++)
--- main-3-2.cpp
+++ main-3-2.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 using namespace std;
+int* readNumbers();
 int *reverseArray(int *numbers1,int length) ;
+bool equalsArray(int* numbers1, int* numbers2, int length);
 int main() {
     int numbers[] = {1, 2, 3, 4, 5};
     int length = sizeof(numbers) / sizeof(numbers[0]);
