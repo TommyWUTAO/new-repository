8e3c8543f04094cd7493236bdf81e401cfdb6ccc
tommy wu <tommywu@tommydeMacBook-Pro.local>
2024-08-12 16:31:55 +0930

    dqw

--- function-3-2.cpp
+++ function-3-2.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 using namespace std;
+
 int* readNumbers() {
-    int* numbers = new int[5];
-    for (int i = 0; i < 5; ++i) {
+    int* numbers = new int[10];
+    for (int i = 0; i < 10; ++i) {
         std::cin >> numbers[i];
     }
     return numbers;
@@ -19,15 +20,17 @@ bool equalsArray(int* numbers1, int* numbers2, int length) {
     }
     return true;
 }
-int *reverseArray(int *numbers1,int length){
-    int* arr=new int[length];
-    for(int i=0;i<length;i++)
-    {
-        arr[i]=numbers1[length-i-1];
+
+int* readNumbers();
+bool equalsArray(int* numbers1, int* numbers2, int length);
+
+int* reverseArray(int* numbers1, int length) {
+    int* arr = new int[length];  // 动态分配一个新数组
+
+    // 反转数组
+    for (int i = 0; i < length; i++) {
+        arr[i] = numbers1[length - i - 1];
     }
-   for(int i=0;i<length;i++)
-   {
-    cout<<arr[i];
-   }
-    return arr;
+
+    return arr;  // 返回反转后的数组
 }
--- main-3-2.cpp
+++ main-3-2.cpp
@@ -1,23 +1,26 @@
 #include <iostream>
 using namespace std;
+
 int* readNumbers();
-int *reverseArray(int *numbers1,int length) ;
+int* reverseArray(int* numbers1, int length);
 bool equalsArray(int* numbers1, int* numbers2, int length);
+
 int main() {
-    int numbers1[]={1,2,3,4,5};
-    int numbers2[]={1,2,3,4,5};
-    cout<<equalsArray(numbers1,numbers2,5);
-    int numbers3[]={4,5};
-    int numbers4[]={1,2};
-    cout<<equalsArray(numbers3,numbers4,2);
+    cout << "Enter 10 numbers:" << endl;
+    int* numbers = readNumbers();  // 读取用户输入的数组
 
-    int numbers[] = {1, 2, 3, 4, 5};
-    int length = sizeof(numbers) / sizeof(numbers[0]);
+    int* reversedNumbers = reverseArray(numbers, 10);  // 反转数组
 
-    int* reversed = reverseArray(numbers, length);
+    bool areEqual = equalsArray(numbers, reversedNumbers, 10);  // 比较原数组和反转后的数组
 
+    if (areEqual) {
+        cout << "The original and reversed arrays are equal." << endl;
+    } else {
+        cout << "The original and reversed arrays are not equal." << endl;
+    }
 
-    delete[] reversed; 
+    delete[] numbers;  // 释放动态分配的内存
+    delete[] reversedNumbers;
 
     return 0;
 }
