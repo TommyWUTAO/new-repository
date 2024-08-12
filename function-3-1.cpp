b8f867f37f044f6872a58b2811bb3ccb41ce8e6e
tommy wu <tommywu@tommydeMacBook-Pro.local>
2024-08-12 15:10:04 +0930

    dq

--- function-3-1.cpp
+++ function-3-1.cpp
@@ -1,22 +1,70 @@
 #include <iostream>
+#include <string>
 
-bool is_fanarray(int array[], int n) {
-    
-    if (n < 1) return false;
+using namespace std;
 
-    
-    for (int i = 0; i < n / 2; ++i) {
-        if (array[i] != array[n - 1 - i]) {
-            return false;
-        }
+class Student {
+private:
+    string name;
+    int ID;
+    double grade;
+
+public:
+    // 构造函数
+    Student(string s_name, int s_ID, double s_grade);
+
+    // Getter方法
+    string get_name() { return name; }
+    int get_ID() { return ID; }
+    double get_grade() { return grade; }
+
+    // Setter方法
+    void change_name(string new_name) { name = new_name; }
+    void change_ID(int new_id) { ID = new_id; }
+    void change_grade(double new_grade) { grade = new_grade; }
+
+    // 判断是否通过的方法
+    bool ispass();
+
+    // 打印学生信息
+    void print() {
+        cout << "Name: " << name << ", ID: " << ID << ", Grade: " << grade << endl;
+    }
+};
+
+// 构造函数定义
+Student::Student(string s_name, int s_ID, double s_grade) {
+    name = s_name;
+    ID = s_ID;
+    grade = s_grade;
+}
+
+// ispass() 方法定义（类的外部）
+bool Student::ispass() {
+    return grade >= 60;
+}
+
+int main() {
+    // 创建Student对象并初始化
+    Student s1("Tom", 123123, 92);
+
+    // 打印学生信息
+    s1.print();
+
+    // 修改学生信息
+    s1.change_name("Jerry");
+    s1.change_ID(456456);
+    s1.change_grade(85);
+
+    // 再次打印学生信息
+    s1.print();
+
+    // 判断是否通过并输出结果
+    if (s1.ispass()) {
+        cout << "Pass" << endl;
+    } else {
+        cout << "Fail" << endl;
     }
 
-    
-    for (int i = 0; i < n / 2; ++i) {
-        if (array[i] > array[i + 1]) {
-            return false;
-        }
-    }
-
-    return true;
+    return 0;
 }
