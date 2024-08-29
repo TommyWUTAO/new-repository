2365f4b45c2552c4317f1366c4454d49c3b47073
TommyWUTAO <tommytao169@gmail.com>
2024-08-29 17:52:30 +0930

    as

--- main-1-3.cpp
+++ main-1-3.cpp
@@ -14,7 +14,7 @@ int main() {
     Vehicle* car1 = new Car(1);
     Vehicle* bus1 = new Bus(2);
     Vehicle* motorbike1 = new Motorbike(3);
-   
-    std::cout << "Number of vehicles parked for more than 15 seconds: " <<  std::endl;
+    int overstayingVehicle = lot.countOverStayingVehicle(2);
+    std::cout << "Number of vehicles parked for more than 15 seconds: " << overstayingVehicles << std::endl;
 
 }
